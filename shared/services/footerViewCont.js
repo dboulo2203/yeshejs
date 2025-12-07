8c02460a9313bcb05971e284835edb517b113834
dboulo2203 <d.boulore@orange.fr>
2025-12-07 13:01:22 +0100

    5.0.4 v3

--- shared/services/footerViewCont.js
+++ shared/services/footerViewCont.js
@@ -8,7 +8,7 @@ export function footerViewDisplay(htlmPartId) {
     <div id="footerPart" style="margin-top:40px">
         <hr style="color:grey"></hr>
         <div class="d-flex justify-content-center" style="">
-            <small>Powered by Dhagpo Kagyu Librairies - Yeshe App - v 5.0.2 - 01/12/2025</small>
+            <small>Powered by Dhagpo Kagyu Librairies - Yeshe App - v 5.0.4 - 07/12/2025</small>
         </div>
         <hr style="color:grey"></hr>
     </div>
